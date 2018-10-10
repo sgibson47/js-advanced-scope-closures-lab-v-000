1be14355fc9a4135f868bdebb5ec369beaa9c971
sgibson47 <sgibson47@gmail.com>
2018-10-10 17:00:16 -0500

    Done.

--- index.js
+++ index.js
@@ -8,6 +8,21 @@ function produceDrivingRange(blocks){
       return `within range by ${blocks - (endNum - startNum)}`
     };
   };
+
+  //OR 
+  // return function(startingBlock, endingBlock){
+
+  //   let start = parseInt(startingBlock);
+  //   let end = parseInt(endingBlock);
+  //   let distanceToTravel = Math.abs(end - start);
+  //   let difference = blockRange - distanceToTravel;
+
+  //   if( difference > 0){
+  //     return `within range by ${difference}`
+  //   } else {
+  //     return `${Math.abs(difference)} blocks out of range`
+  //   }
+  // }
 }
 
 function produceTipCalculator(tip){
